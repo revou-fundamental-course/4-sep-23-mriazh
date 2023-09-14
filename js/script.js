dea6e77bf70f4a3d04ddb0c0575b92974d9b9a57
Riaz <mriyadhazhar@gmail.com>
2023-09-14 23:59:54 +0700

    go

--- js/script.js
+++ js/script.js
@@ -1,42 +1,41 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const squareForm = document.getElementById("squareForm");
-    const sideLength = document.getElementById("sideLength");
-    const calc = document.getElementById("calc");
-    const result = document.getElementById("result");
-    const perimeterResult = document.getElementById("perimeterResult");
-    const areaResult = document.getElementById("areaResult");
-    const option = document.getElementById("option");
+document.addEventListener('DOMContentLoaded', function () {
+    const squareForm = document.getElementById('squareForm');
+    const sideLengthInput = document.getElementById('sideLength');
+    const calculateButton = document.getElementById('calculateButton');
+    const resultSection = document.getElementById('resultSection');
+    const areaResult = document.getElementById('areaResult');
+    const perimeterResult = document.getElementById('perimeterResult');
+    const menuSection = document.getElementById('menuSection');
 
-    squareForm.addEventListener("submit", function (e) {
+    squareForm.addEventListener('submit', function (e) {
         e.preventDefault();
-        const side = parseFloat(sideLength.value);
-        const checkedOption = document.querySelector("input[name='operation']:checked").value;
+        const sideLength = parseFloat(sideLengthInput.value);
+        const selectedOperation = document.querySelector('input[name="operation"]:checked').value;
 
-        if (isNaN(side)) {
-            if (checkedOption === "perimeter") {
-                const perimeter = 4 * side;
-                perimeterResult.textContent = perimeter.toFixed(2);
-                areaResult.textContent = "";
-            }
-            else if (checkedOption === "area") {
-                const area = side * side;
+        if (!isNaN(sideLength)) {
+            if (selectedOperation === 'area') {
+                const area = sideLength * sideLength;
                 areaResult.textContent = area.toFixed(2);
-                perimeterResult.textContent = "";
+                perimeterResult.textContent = ''; // Clear the perimeter result
+            } else if (selectedOperation === 'perimeter') {
+                const perimeter = 4 * sideLength;
+                perimeterResult.textContent = perimeter.toFixed(2);
+                areaResult.textContent = ''; // Clear the area result
             }
 
-            result.style.display = "block";
+            resultSection.style.display = 'block';
         }
     });
 
-    option.addEventListener("change", function (){
-        const checkedOption = document.querySelector("input[name='operation']:checked").value;
-        if (checkedOption === "perimeter"){
-            perimeterResult.style.display = "block";
-            areaResult.style.display = "none";
+    // Show/hide the appropriate result based on the selected operation
+    menuSection.addEventListener('change', function () {
+        const selectedOperation = document.querySelector('input[name="operation"]:checked').value;
+        if (selectedOperation === 'area') {
+            areaResult.style.display = 'block';
+            perimeterResult.style.display = 'none';
+        } else if (selectedOperation === 'perimeter') {
+            areaResult.style.display = 'none';
+            perimeterResult.style.display = 'block';
         }
-        else if (checkedOption === "area"){
-            areaResult.style.display = "block";
-            perimeterResult.style.display = "none";
-        }
-    })
-})
+    });
+});
